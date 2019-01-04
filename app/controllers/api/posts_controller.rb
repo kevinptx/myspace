a72e33b61efe39f4a8ed2d440ba4553950faf548
Kevin Peery <kevin.peery@gmail.com>
2019-01-04 09:45:59 -0700

    update rails routes and controllers

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,7 +1,36 @@
 class Api::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, except: [:index]
+
   def index
+    render json: Post.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def create
+    post = @user.posts.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: post.errors
+    end
   end
 
   def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:posts).permit(:body, :likes, :first_name, :last_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/auth'
+  mount_devise_token_auth_for "User", at: "api/auth"
   namespace :api do
-    resources :posts, only: [:index, :update]
+    resources :users, only: [:index] do
+      resources :posts, only: [:create]
+    end
+    resources :posts, only: [:index]
   end
 end
